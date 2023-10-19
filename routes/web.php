12d174395d2faa0ca9c78f18529a61701df1ef2a
Gael Garcia <garcia1234arvizu1234gael@gmail.com>
2023-10-19 11:50:47 -0700

    Que son los Middleware en Laravel 10

--- routes/web.php
+++ routes/web.php
@@ -8,21 +8,26 @@ Route::get('/', function () {
     return "welcome to our home page";
 });
 
-Route::get('/chirps', function () {
-    return "welcome to our chirps page";
+
+
+
+
+
+
+Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    
+    Route::get('/chirps', function () {
+        return "welcome to our chirps page";
+    })->name('chirps.index');
 });
 
-
-
-
-// Route::get('/dashboard', function () {
-//     return view('dashboard');
-// })->middleware(['auth', 'verified'])->name('dashboard');
-
-// Route::middleware('auth')->group(function () {
-//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-// });
-
-// require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
