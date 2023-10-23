191b84f81ec281bf08938c2d0f09023441bfc527
Gael Garcia <garcia1234arvizu1234gael@gmail.com>
2023-10-23 13:33:58 -0700

    Listado de chirps

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -13,7 +13,9 @@ class ChirpController extends Controller
     public function index()
     {
         //
-        return view('chirps.index');
+        return view('chirps.index', [
+            'chirps' => Chirp::latest()->get()
+        ]);
     }
 
     
--- resources/views/chirps/index.blade.php
+++ resources/views/chirps/index.blade.php
@@ -13,12 +13,35 @@
                         @csrf
                         <textarea
                             class="block w-full rounded-md border-gray-300 bg-white shadow-sm transition-colors duration-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:focus:border-indigo-300 dark:focus:ring dark:focus:ring-indigo-200 dark:focus:ring-opacity-50"
-                            name="message" placeholder="{{ __('What\'s on your mind?') }}"
-                            >{{old('message')}}</textarea>
-                        <x-input-error :messages="$errors->get('message')"/>
+                            name="message" placeholder="{{ __('What\'s on your mind?') }}">{{ old('message') }}</textarea>
+                        <x-input-error :messages="$errors->get('message')" />
                         <x-primary-button class="mt-4">{{ __('Chirp') }}</x-primary-button>
                     </form>
                 </div>
+            </div>
+
+            <div class="mt-6 bg-white dark:bg-gray-800 shadow-sm rounded-lg divide-y dark:divide-gry-900">
+                @foreach ($chirps as $chirp)
+                    <div class="p-6 flex space-x-2">
+                        <svg class="h-6 w-6 text-gray-600 dark:text-gray-400 -scale=x=100" fill="none"
+                            stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M8.625 9.75a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H8.25m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H12m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0h-.375m-13.5 3.01c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.184-4.183a1.14 1.14 0 01.778-.332 48.294 48.294 0 005.83-.498c1.585-.233 2.708-1.626 2.708-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z">
+                            </path>
+                        </svg>
+
+                        <div class="flex-1">
+                            <div class="flex justyfy-between items-center">
+                                <div>
+                                    <span class="text-gray-800 dark:text-gray-200">USER_NAME</span>
+                                    <small class="ml-2 text-sm text-gray-600 dark:text-gray-400">{{ $chirp->created_at->format('j M Y, g:i a') }}</small>
+                                </div>
+                            </div>
+                            <p class="mt-4 text-lg text-gray-900 dark:text-gray-100">{{ $chirp->message }}</p>
+                        </div>
+                    </div>
+                @endforeach
             </div>
         </div>
     </div>
